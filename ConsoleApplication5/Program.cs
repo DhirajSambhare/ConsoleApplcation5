0facf0f8e680a45c414dc0594aef76f5bb457764
Dhiraj  Sambhare <dhiraj.sambhare@nihilent.com>
2018-04-02 16:40:19 +0530

    Adding Files

---
+++ ConsoleApplication5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] scores = { 90, 71, 82, 93, 75, 82 };
+
+            // Query Expression.
+            IEnumerable<int> scoreQuery = //query variable
+                from score in scores //required
+                where score > 80 // optional
+                orderby score descending // optional
+                select score; //must end with select or group
+
+            // Execute the query to produce the results
+            //foreach (int testScore in scoreQuery)
+            //{
+            //    Console.WriteLine(testScore);
+            //}
+            IEnumerator enumerator = scoreQuery.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current);
+                // Perform logic on the item
+            }
+
+          
+        }
+    }
+}
